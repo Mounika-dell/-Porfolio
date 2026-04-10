f1f4637230c3b4237a4d2ae4df75b76e20b48eb8
Mounika-dell <mounikareddyrachamalla@gmail.com>
2026-04-10 17:20:59 +0530

    fixed root route

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
 const Contact = require("./models/contact");
 const nodemailer = require("nodemailer");
 
@@ -11,40 +11,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "../frontend")));
-
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+    res.send("Backend is live ✅");
 });
 
 app.post("/contact", async (req, res) => {
     try {
         const newMessage = new Contact(req.body);
         await newMessage.save();
+
         await transporter.sendMail({
-            from: "mounikarachamla@gmail.com",
-            to: "mounikarachamla@gmail.com",
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_USER,
             subject: `Portfolio Contact: ${req.body.subject}`,
             text: `
-        New portfolio message received:
+New portfolio message:
 
-        Name: ${req.body.name}
-        Email: ${req.body.email}
-        Subject: ${req.body.subject}
-        Message: ${req.body.message}
-            `
-});
+Name: ${req.body.name}
+Email: ${req.body.email}
+Subject: ${req.body.subject}
+Message: ${req.body.message}
+`
+        });
 
         res.json({ message: "Message Sent Successfully ✅" });
+
     } catch (error) {
-    console.log("SAVE ERROR:", error);
-    res.status(500).json({ message: "Error saving message ❌" });
-}
+        console.log(error);
+        res.status(500).json({ message: "Error saving message ❌" });
+    }
 });
+
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
@@ -52,8 +53,8 @@ const transporter = nodemailer.createTransport({
         pass: process.env.EMAIL_PASS
     }
 });
-const PORT = process.env.PORT || 5000;
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on ${PORT}`);
 });
